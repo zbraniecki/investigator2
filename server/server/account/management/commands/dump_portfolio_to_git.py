254fb0dee6635a4e7f9cbbdfd21ac0be0ab85dce
Zibi Braniecki <zibi@braniecki.net>
2021-10-30 01:06:05 -0500

    Improve dumping portfolio

--- server/server/account/management/commands/dump_portfolio_to_git.py
+++ server/server/account/management/commands/dump_portfolio_to_git.py
@@ -13,6 +13,55 @@ import toml
 
 getcontext().prec = 2
 
+PROVIDER_MAP = {
+    "algorand": "myalgo",
+    "avalanche": "avawallet",
+    "binance-us": "binance.us",
+    "coinbase-pro": "coinbasepro",
+    "icp-nns": "icp/nns",
+    "oasis": "oasis_wallet",
+}
+
+SYMBOL_MAP = {
+    "avax": "AVAX",
+}
+
+
+def normalize_symbol(input):
+    if input.lower() in SYMBOL_MAP:
+        return SYMBOL_MAP[input.lower()]
+    else:
+        return input
+
+
+def normalize_provider(input):
+    if input.lower() in PROVIDER_MAP:
+        return PROVIDER_MAP[input.lower()]
+    else:
+        return input
+
+
+def sort(obj):
+    obj["holding"].sort(
+        key=lambda item: (item["wallet"], item["quantity"], item["symbol"])
+    )
+
+
+def compare_to_reference(path, result):
+    reference = toml.load(f"{path}/account/portfolio.toml")
+    sort(reference)
+    sort(result)
+    assert len(reference["holding"]) == len(result["holding"])
+    # print(toml.dumps(reference))
+    # print("====")
+    print(toml.dumps(result))
+    # for (i1, i2) in zip(reference["holding"], result["holding"]):
+    #     if i1 == i2:
+    #         continue
+    #     print("Diff between items:")
+    #     print(i1)
+    #     print(i2)
+
 
 class Command(BaseCommand):
     help = "Dump portfolio data to git"
@@ -29,12 +78,14 @@ class Command(BaseCommand):
 
         result = []
         for holding in holdings:
-            wallet = holding.wallet.service.provider.id
+            wallet = normalize_provider(holding.wallet.service.provider.id)
             entry = {
-                "symbol": holding.asset.symbol,
+                "symbol": normalize_symbol(holding.asset.symbol),
                 "quantity": float(holding.quantity),
                 "wallet": wallet,
             }
             result.append(entry)
-        # print(json.dumps(result, indent=2))
-        print(toml.dumps({"holding": result}))
+        result = {"holding": result}
+        # compare_to_reference(path, result)
+        result = toml.dumps(result)
+        print(result)
