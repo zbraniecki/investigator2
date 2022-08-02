942c77eb526a82d2f0f210b8ec4b600197aeb485
Zibi Braniecki <zibi@unicode.org>
2022-08-02 17:13:50 +0100

    Add fetch_assets entries

--- server/investigator/oracle/management/commands/fetch_assets.py
+++ server/investigator/oracle/management/commands/fetch_assets.py
@@ -49,7 +49,7 @@ COINS = [
     "uma",
     ["usdc", "usd-coin"],
     ["usdt", "tether"],
-    ["reef", "reef-finance"],
+    ["reef", "reef"],
     ["gs", "genesis-shards"],
     ["powr", "power-ledger"],
     ["nu", "nucypher"],
@@ -80,6 +80,9 @@ COINS = [
     ["kar", "karura"],
     ["lit", "litentry"],
     ["rndr", "render-token"],
+    "vite",
+    ["aca", "acala"],
+    ["azero", "aleph-zero"],
 ]
 
 COIN_LIST_URL = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=%PAGE%&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C30d"
